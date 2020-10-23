accffcbf30c2d0092ae1e153efdfe44344fd48ec
SBuchanan1 <syd.buch@gmail.com>
2020-10-22 22:44:18 -0400

    updated server.js

--- server.js
+++ server.js
@@ -10,14 +10,31 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker" {
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+
 });
 
-// routes
-app.use(require("./routes/api.js"));
+// // routes
+app.use(require(".routes/html-routes"));
+app.use(require(".routes/html-routes"))
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
+});
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+    console.log("Mongoose successfully connected.");
+});
+
+connection.on("error", (err) => {
+    console.log("Mongoose connection error: ", err);
+});
+
+app.use(workoutController);
+require("./controllers/htmlRoutes")(app);
+app.listen(PORT, function () {
+    console.log("Listening at http://localhost:" + PORT);
 });
