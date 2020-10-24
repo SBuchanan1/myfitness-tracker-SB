f505af767b860b8732e3328a917fadc1e3a82a0f
SBuchanan1 <syd.buch@gmail.com>
2020-10-23 23:06:33 -0400

    added apiroutes

--- server.js
+++ server.js
@@ -1,43 +1,25 @@
 const express = require("express");
+const morgan = require("morgan");
 const mongoose = require("mongoose");
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
-
-// const ingredientsController = require("./controllers/ingredientsController");
-// const pizzaController = require("./controllers/pizzaController")
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    }
-);
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
 
-const connection = mongoose.connection;
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-connection.on("connected", () => {
-    console.log("Mongoose successfully connected.");
+app.listen(PORT, function () {
+    console.log(`App listening on Port ${PORT}`);
 });
-
-connection.on("error", (err) => {
-    console.log("Mongoose connection error: ", err);
-});
-
-app.get("/api/config", (req, res) => {
-    res.json({
-        success: true,
-    });
-});
-
-app.use(ingredientsController);
-app.use(pizzaController);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
