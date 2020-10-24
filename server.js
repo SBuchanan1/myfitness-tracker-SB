5c481bb4e1884d75134ab6f23957f4674146a6c9
SBuchanan1 <syd.buch@gmail.com>
2020-10-23 23:24:54 -0400

    modified the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const morgan = require("morgan");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
